defb0e56695cf01d64b4177cda9617dea020ab15
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-01-08 06:59:54 -0300

    due

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Hosting;
+ï»¿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace Shop
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-        
+        public static void Main(string[] args) => CreateHostBuilder(args).Build()
+            .Run();
 
-        //Abstração de self host
+
+        //AbstraÃ§Ã£o de self host
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
